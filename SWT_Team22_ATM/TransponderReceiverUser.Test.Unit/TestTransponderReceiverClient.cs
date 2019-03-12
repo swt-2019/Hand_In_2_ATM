4be8e5b084e1ecdb9cd28ce48ad09a51a9af8f74
Marcus Gasberg <mrgasberg@hotmail.com>
2019-03-12 16:52:21 +0100

    Added the TransponderReceiver Project and TestProject.
    Wrote some tests for the TransponderReceiver

---
+++ SWT_Team22_ATM/TransponderReceiverUser.Test.Unit/TestTransponderReceiverClient.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using NSubstitute;
+using NUnit.Framework;
+using TransponderReceiver;
+
+namespace TransponderReceiverUser.Test.Unit
+{
+    public class TestTransponderReceiverClient
+    {
+        private ITransponderReceiver _fakeTransponderReceiver;
+        private TransponderReceiverClient _uut;
+        [SetUp]
+        public void Setup()
+        {
+            // Make a fake Transponder Data Receiver
+            _fakeTransponderReceiver = Substitute.For<ITransponderReceiver>();
+            // Inject the fake TDR
+            _uut = new TransponderReceiverClient(_fakeTransponderReceiver);
+        }
+
+        [Test]
+        public void TransponderReceiverClient_TransponderReceiverReceivesMultipleData_ClientGetsRightData()
+        {
+            // Setup test data
+            List<string> testData = new List<string>();
+            testData.Add("ATR423;39045;12932;14000;20151006213456789");
+            testData.Add("BCD123;10005;85890;12000;20151006213456789");
+            testData.Add("XYZ987;25059;75654;4000;20151006213456789");
+
+            // Act: Trigger the fake object to execute event invocation
+            _fakeTransponderReceiver.TransponderDataReady
+                += Raise.EventWith(this, new RawTransponderDataEventArgs(testData));
+
+            // Assert: What is received is actually the test data
+            Assert.That(_uut.TransponderData,Is.EqualTo(testData));
+        }
+
+
+        [Test]
+        public void TransponderReceiverClient_TransponderReceiverReceivesEmptyData_ClientGetsEmptyData()
+        {
+            // Setup: Empty test data
+            List<string> testData = new List<string>();
+
+            // Act: Trigger the fake object to execute event invocation
+            _fakeTransponderReceiver.TransponderDataReady
+                += Raise.EventWith(this, new RawTransponderDataEventArgs(testData));
+            
+            //Assert: TransponderData Is Empty
+            Assert.That(_uut.TransponderData, Is.Empty);
+        }
+    }
+}
---
+++ SWT_Team22_ATM/TransponderReceiverUser/TransponderReceiverClient.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using TransponderReceiver;
+
+namespace TransponderReceiverUser
+{
+    public class TransponderReceiverClient
+    {
+        private ITransponderReceiver receiver;
+
+        // Using constructor injection for dependency/ies
+        public TransponderReceiverClient(ITransponderReceiver receiver)
+        {
+            // This will store the real or the fake transponder data receiver
+            this.receiver = receiver;
+
+            // Attach to the event of the real or the fake TDR
+            this.receiver.TransponderDataReady += ReceiverOnTransponderDataReady;
+        }
+
+        public List<string> TransponderData { get; set; }
+
+        protected void ReceiverOnTransponderDataReady(object sender, RawTransponderDataEventArgs e)
+        {
+            TransponderData = e.TransponderData;
+        }
+    }
+}
