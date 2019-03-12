278d84989ad100dad3f240dd7b8b89bda9d1ba3b
Marcus Gasberg <mrgasberg@hotmail.com>
2019-03-12 13:23:11 +0100

    Create dummy test

--- SWT_Team22_ATM/SWT_TEAM22_ATM.Test.Unit/TransponderTests.cs
+++ SWT_Team22_ATM/SWT_TEAM22_ATM.Test.Unit/TransponderTests.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace SWT_TEAM22_ATM.Test.Unit
 {
-    public class Class1
+    [TestFixture]
+    public class TransponderTests
     {
+        [Test]
+        void DummyTest()
+        {
+            Assert.True(true);
+        }
     }
 }
