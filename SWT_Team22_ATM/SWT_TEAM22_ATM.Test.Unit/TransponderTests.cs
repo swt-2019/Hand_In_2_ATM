ca5cd8948429729337a65de0ce4a7e063040bf06
Marcus Gasberg <mrgasberg@hotmail.com>
2019-03-12 13:07:22 +0100

    Added test project and Transponder test file

---
+++ SWT_Team22_ATM/SWT_TEAM22_ATM.Test.Unit/TransponderTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWT_TEAM22_ATM.Test.Unit
+{
+    public class Class1
+    {
+    }
+}
