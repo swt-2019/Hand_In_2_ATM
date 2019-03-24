18d6a1f4afab918406e2ee54c14df2d6c4be4fd4
Tobias Mønster <au572284@uni.au.dk>
2019-03-24 18:33:38 +0100

    console app done

--- SWT_Team22_ATM/SWT_Team22_ATM_ConsoleApp/Program.cs
+++ SWT_Team22_ATM/SWT_Team22_ATM_ConsoleApp/Program.cs
@@ -9,7 +9,8 @@ using SWT_Team22_ATM.interpreter;
 using SWT_Team22_ATM.Monitors;
 using SWT_Team22_ATM.ConditionDetector;
 using SWT_Team22_ATM.Validation;
-using SWT_Team22_ATM;
+using SWT_Team22_ATM.Updater;
+using SWT_Team22_ATM.Updater.ICalculateTracks;
 
 using TransponderReceiver;
 
@@ -41,26 +42,36 @@ namespace SWT_Team22_ATM_ConsoleApp
             TDInterpreter.subscribe(tr);
 
             Position p = new Position();
-            p.XCoordinate = 0;
-            p.YCoordinate = 0;
-            p.ZCoordinate = 0;
+            p.XCoordinate = 30000;
+            p.YCoordinate = 30000;
+            p.ZCoordinate = 5;
 
             airspace = new Airspace();
             airspace.AirspacePosition = p;
+            airspace.HorizontalSize = 100000;
+            airspace.VerticalEnd = 100000;
 
             ITrackListEvent itlEvent = TDInterpreter;
             validator = new ValidateTransponderData(ref itlEvent , airspace);
 
             AirOutputter = new AirTrafficOutputter();
+            AirOutputter.TrafficController = new ConsoleAirTrafficController();
             List<IConditionStrategy<ITrack>> conditionStrategies = new List<IConditionStrategy<ITrack>>();
-            conditionStrategies.Add(new TrackAltitudeCondition());
-            conditionStrategies.Add(new TrackHorizontalDistanceCondition());
+
+            conditionStrategies.Add(new TrackAltitudeCondition() { AltitudeRestriction = 100000 });
+            conditionStrategies.Add(new TrackHorizontalDistanceCondition() { HorizontalDistance = 100000 });
 
             airCondDetector = new AirspaceTrackConditionDetector(conditionStrategies);
 
-            Monitor = new AirTrafficMonitor(validator, airCondDetector,AirOutputter);
+            TrackHorizontalDistanceCalculator HorizontalDistanceCalc = new TrackHorizontalDistanceCalculator();
+            TrackSpeedCalculator speedCalc = new TrackSpeedCalculator(HorizontalDistanceCalc);
+            TrackCourseCalculator courseCalc = new TrackCourseCalculator();
+            AirspaceUpdater airUpdater = new AirspaceUpdater(speedCalc,courseCalc);
+            
+
+            Monitor = new AirTrafficMonitor(validator, airCondDetector,AirOutputter,airUpdater);
             FileLogger fl = new FileLogger();
-            fl.PathToFile = "~/log.txt";
+            fl.PathToFile = "../../log.txt";
             AirOutputter.Logger = fl;
         }
 
